39744ad5d160dba669fd4b6f194c01bb4fb3f1f4
Anmol_Singh <anmolsingh9822@gmail.com>
2021-09-27 10:55:05 +0530

    started with web tech

--- Compiler Design/exp1.py
+++ Compiler Design/exp1.py
@@ -1,28 +1,25 @@
-import os
-raw_original=input("Enter the expression with space: ")
-print("your Input is: "+raw_original)
-original=raw_original.split(" ")
+raw_input=input("Enter the expression with space: ")
+print("your Input is: "+raw_input)
+original=raw_input.split(" ")
 num=[]
 sign=[]
-flag=0
-flag=0
+counter=0
 for i in original:
     issign=True if i=='+' or i=='-' else False
-    isnum=False
-    if(issign==False):
-        isnum= True if int(i)<=9 or int(i)>=0 else False
-    if(isnum and issign==False):
-        if(int(original[flag])<9 and int(original[flag])>0):
-            num.append(int(original[flag]))
-            flag+=1
+    if(issign==False and int(i)>=0 and int(i)<=9):
+        num.append(int(original[counter]))
+        counter+=1
+    elif(issign==True):
+        sign.append(original[counter])
+        counter+=1
     else:
-        sign.append(original[flag])
-        flag+=1
+        sign.append(original[counter])
+        counter+=1
 
-if len(num)!=(len(sign)-1):
+if len(num)==(len(sign)-1):
     print("You entered a wrong expression. Please make sure it is a mathematical expression...")
 
 
-print("Your Postfix expression is: "+str(num)+" "+str(sign))
+print("Output: "+str(num[0])+str(num[1])+str(sign[0])+str(num[2])+str(sign[1]))
 
 
